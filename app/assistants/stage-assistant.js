27ff778180160e94a6c2fb832d8597aee76af1f2
Mark Paschal <markpasc@markpasc.org>
2010-05-12 11:24:37 -0700

    Try to respond to the timer launching us

--- app/assistants/stage-assistant.js
+++ app/assistants/stage-assistant.js
@@ -6,3 +6,9 @@ StageAssistant.prototype.setup = function() {
     /* this function is for setup tasks that have to happen when the stage is first created */
     this.controller.pushScene('settings');
 };
+
+StageAssistant.prototype.handleLaunch = function (parameters) {
+    if (parameters.action == 'swappaper') {
+        Mojo.Controller.getAppController().showBanner('O HAI SWAPPY PAPER', {source:'notification'});
+    }
+};
